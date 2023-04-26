24b8156ae52087a967efb4f889deb5b03e47ea8d
Stas <113462863+shamansk1y@users.noreply.github.com>
2023-04-26 16:07:27 +0200

    Update send_emails.py

--- send_emails.py
+++ send_emails.py
@@ -56,26 +56,26 @@ if users_dct:
             msg.attach_alternative(_html, "text/html")
             msg.send()
 
-qs = Error.objects.filter(timestamp=today)
-subject = ''
-text_content = ''
-to = ADMIN_USER
-_html = ''
-if qs.exists():
-    error = qs.first()
-    data = error.get('errors', [])
-    for i in data:
-        _html += f'<p"><a href="{ i["url"] }">Error: { i["title"] }</a></p><br>'
-    subject += f"Ошибки скрапинга {today}"
-    text_content += "Ошибки скрапинга"
-    data = error.data.get('user_data')
-    if data:
-        _html += '<hr>'
-        _html += '<h2>Пожелания пользователей </h2>'
-        for i in data:
-            _html += f'<p">Город: {i["city"]}, Специальность:{i["language"]},  Имейл:{i["email"]}</p><br>'
-        subject += f" Пожелания пользователей {today}"
-        text_content += "Пожелания пользователей"
+# qs = Error.objects.filter(timestamp=today)
+# subject = ''
+# text_content = ''
+# to = ADMIN_USER
+# _html = ''
+# if qs.exists():
+#     error = qs.first()
+#     data = error.data.get('errors', [])
+#     for i in data:
+#         _html += f'<p"><a href="{ i["url"] }">Error: { i["title"] }</a></p><br>'
+#     subject += f"Ошибки скрапинга {today}"
+#     text_content += "Ошибки скрапинга"
+#     data = error.data.get('user_data')
+#     if data:
+#         _html += '<hr>'
+#         _html += '<h2>Пожелания пользователей </h2>'
+#         for i in data:
+#             _html += f'<p">Город: {i["city"]}, Специальность:{i["language"]},  Имейл:{i["email"]}</p><br>'
+#         subject += f" Пожелания пользователей {today}"
+#         text_content += "Пожелания пользователей"
 
 qs = Url.objects.all().values('city', 'language')
 urls_dct = {(i['city'], i['language']): True for i in qs}
@@ -95,21 +95,4 @@ if subject:
     msg.attach_alternative(_html, "text/html")
     msg.send()
 
-# import smtplib
-# from email.mime.multipart import MIMEMultipart
-# from email.mime.text import MIMEText
-#
-# msg = MIMEMultipart('alternative')
-# msg['Subject'] = 'Список вакансий за  {}'.format(today)
-# msg['From'] = EMAIL_HOST_USER
-# mail = smtplib.SMTP()
-# mail.connect(EMAIL_HOST, 25)
-# mail.ehlo()
-# mail.starttls()
-# mail.login(EMAIL_HOST_USER, EMAIL_HOST_PASSWORD)
-#
-# html_m = "<h1>Hello world</h1>"
-# part = MIMEText(html_m, 'html')
-# msg.attach(part)
-# mail.sendmail(EMAIL_HOST_USER, [to], msg.as_string())
-# mail.quit()
+
